860e4632180c34f01983a6dfcc3f19ec14ee43d7
Patrick Williams <pwmckenna@gmail.com>
2014-01-10 09:38:05 -0800

    making the promise chain logic much sturdier

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var TravisGenerator = require('../lib/travis-generator');
 var path = require('path');
 var util = require('util');
+var q = require('q');
 
 module.exports = Generator;
 
@@ -16,53 +17,93 @@ Generator.prototype.writeDotTravisFile = function () {
     var done = this.async();
     this.displayLogo()
         .then(function () {
-            return this.initializeGitHubApi()
-            .then(this.celebrate('Initialize GitHub Api'), this.mourn('Initialize GitHub Api'));
+            return q.resolve().then(function () {
+                return this.initializeGitHubApi();
+            }.bind(this)).then(
+                this.celebrate('Initialize GitHub Api'),
+                this.mourn('Initialize GitHub Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.initializeTravisApi()
-            .then(this.celebrate('Initialize Travis-ci Api'), this.mourn('Initialize Travis-ci Api'));
+            return q.resolve().then(function () {
+                return this.initializeTravisApi();
+            }.bind(this)).then(
+                this.celebrate('Initialize Travis-ci Api'),
+                this.mourn('Initialize Travis-ci Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.repositoryInformation()
-            .then(this.celebrate('Query Repository Information'), this.mourn('Query Repository Information'));
+            return q.resolve().then(function () {
+                return this.repositoryInformation();
+            }.bind(this)).then(
+                this.celebrate('Query Repository Information'),
+                this.mourn('Query Repository Information')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.gitHubLogin()
-            .then(this.celebrate('Login to GitHub Api'), this.mourn('Login to GitHub Api'));
+            return q.resolve().then(function () {
+                return this.gitHubLogin();
+            }.bind(this)).then(
+                this.celebrate('Login to GitHub Api'),
+                this.mourn('Login to GitHub Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.ensureTravisAppAuthorized()
-            .then(this.celebrate('Ensure GitHub Travis App Authorized'), this.mourn('Ensure GitHub Travis App Authorized'));
+            return q.resolve().then(function () {
+                return this.ensureTravisAppAuthorized();
+            }.bind(this)).then(
+                this.celebrate('Ensure GitHub Travis App Authorized'),
+                this.mourn('Ensure GitHub Travis App Authorized')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.generateGitHubOAuthToken()
-            .then(this.celebrate('Generate GitHub OAuth Token'), this.mourn('Generate GitHub OAuth Token'));
+            return q.resolve().then(function () {
+                return this.generateGitHubOAuthToken();
+            }.bind(this)).then(
+                this.celebrate('Generate GitHub OAuth Token'),
+                this.mourn('Generate GitHub OAuth Token')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.travisGitHubAuthentication()
-            .then(this.celebrate('Login to Travis-ci Api'), this.mourn('Login to Travis-ci Api'));
+            return q.resolve().then(function () {
+                return this.travisGitHubAuthentication();
+            }.bind(this)).then(
+                this.celebrate('Login to Travis-ci Api'),
+                this.mourn('Login to Travis-ci Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.ensureTravisRepositoryHookSet()
-            .then(this.celebrate('Ensure Travis Repository Hook Set'), this.mourn('Ensure Travis Repository Hook Set'));
+            return q.resolve().then(function () {
+                return this.ensureTravisRepositoryHookSet();
+            }.bind(this)).then(
+                this.celebrate('Ensure Travis Repository Hook Set'),
+                this.mourn('Ensure Travis Repository Hook Set')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.insertReadmeStatusImage()
-            .then(this.celebrate('Readme Build Status Image'), this.mourn('Readme Build Status Image'));
+            return q.resolve().then(function () {
+                return this.insertReadmeStatusImage();
+            }.bind(this)).then(
+                this.celebrate('Readme Build Status Image'),
+                this.mourn('Readme Build Status Image')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.revokeGitHubOAuthToken()
-            .then(this.celebrate('Revoke GitHub OAuth Token'), this.mourn('Revoke GitHub OAuth Token'));
+            return q.resolve().then(function () {
+                return this.revokeGitHubOAuthToken();
+            }.bind(this)).then(
+                this.celebrate('Revoke GitHub OAuth Token'),
+                this.mourn('Revoke GitHub OAuth Token')
+            );
         }.bind(this))
 
         .then(function () {
--- gh-pages/index.js
+++ gh-pages/index.js
@@ -19,68 +19,120 @@ Generator.prototype.writeDotTravisFile = function () {
     this.displayLogo()
 
         .then(function () {
-            return this.initializeGitHubApi()
-                .then(this.celebrate('Initialize GitHub Api'), this.mourn('Initialize GitHub Api'));
+            return q.resolve().then(function () {
+                return this.initializeGitHubApi();
+            }.bind(this)).then(
+                this.celebrate('Initialize GitHub Api'),
+                this.mourn('Initialize GitHub Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.initializeTravisApi()
-                .then(this.celebrate('Initialize Travis-ci Api'), this.mourn('Initialize Travis-ci Api'));
+            return q.resolve().then(function () {
+                return this.initializeTravisApi();
+            }.bind(this)).then(
+                this.celebrate('Initialize Travis-ci Api'),
+                this.mourn('Initialize Travis-ci Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.repositoryInformation()
-                .then(this.celebrate('Query Repository Information'), this.mourn('Query Repository Information'));
+            return q.resolve().then(function () {
+                return this.repositoryInformation();
+            }.bind(this)).then(
+                this.celebrate('Query Repository Information'),
+                this.mourn('Query Repository Information')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.setSourceBranch()
-                .then(this.celebrate('Set Source Branch'), this.mourn('Set Source Branch'));
+            return q.resolve().then(function () {
+                return this.setSourceBranch();
+            }.bind(this)).then(
+                this.celebrate('Set Source Branch'),
+                this.mourn('Set Source Branch')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.setDestinationBranch()
-                .then(this.celebrate('Set Destination Branch'), this.mourn('Set Destination Branch'));
+            return q.resolve().then(function () {
+                return this.setDestinationBranch();
+            }.bind(this)).then(
+                this.celebrate('Set Destination Branch'),
+                this.mourn('Set Destination Branch')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.gitHubLogin()
-                .then(this.celebrate('Login to GitHub Api'), this.mourn('Login to GitHub Api'));
+            return q.resolve().then(function () {
+                return this.gitHubLogin();
+            }.bind(this)).then(
+                this.celebrate('Login to GitHub Api'),
+                this.mourn('Login to GitHub Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.gitHubUserInfo()
-                .then(this.celebrate('Query GitHub User Information'), this.mourn('Query GitHub User Information'));
+            return q.resolve().then(function () {
+                return this.gitHubUserInfo();
+            }.bind(this)).then(
+                this.celebrate('Query GitHub User Information'),
+                this.mourn('Query GitHub User Information')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.ensureTravisAppAuthorized()
-                .then(this.celebrate('Ensure GitHub Travis App Authorized'), this.mourn('Ensure GitHub Travis App Authorized'));
+            return q.resolve().then(function () {
+                return this.ensureTravisAppAuthorized();
+            }.bind(this)).then(
+                this.celebrate('Ensure GitHub Travis App Authorized'),
+                this.mourn('Ensure GitHub Travis App Authorized')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.generateGitHubOAuthToken()
-                .then(this.celebrate('Generate GitHub OAuth Token'), this.mourn('Generate GitHub OAuth Token'));
+            return q.resolve().then(function () {
+                return this.generateGitHubOAuthToken();
+            }.bind(this)).then(
+                this.celebrate('Generate GitHub OAuth Token'),
+                this.mourn('Generate GitHub OAuth Token')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.travisGitHubAuthentication()
-                .then(this.celebrate('Login to Travis-ci Api'), this.mourn('Login to Travis-ci Api'));
+            return q.resolve().then(function () {
+                return this.travisGitHubAuthentication();
+            }.bind(this)).then(
+                this.celebrate('Login to Travis-ci Api'),
+                this.mourn('Login to Travis-ci Api')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.ensureTravisRepositoryHookSet()
-                .then(this.celebrate('Ensure Travis Repository Hook Set'), this.mourn('Ensure Travis Repository Hook Set'));
+            return q.resolve().then(function () {
+                return this.ensureTravisRepositoryHookSet();
+            }.bind(this)).then(
+                this.celebrate('Ensure Travis Repository Hook Set'),
+                this.mourn('Ensure Travis Repository Hook Set')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.insertReadmeStatusImage()
-                .then(this.celebrate('Readme Build Status Image'), this.mourn('Readme Build Status Image'));
+            return q.resolve().then(function () {
+                return this.insertReadmeStatusImage();
+            }.bind(this)).then(
+                this.celebrate('Readme Build Status Image'),
+                this.mourn('Readme Build Status Image')
+            );
         }.bind(this))
 
         .then(function () {
-            return this.encryptGitHubOAuthToken()
-                .then(this.celebrate('Encrypt GitHub OAuth Token'), this.mourn('Encrypt GitHub OAuth Token'));
+            return q.resolve().then(function () {
+                return this.encryptGitHubOAuthToken();
+            }.bind(this)).then(
+                this.celebrate('Encrypt GitHub OAuth Token'),
+                this.mourn('Encrypt GitHub OAuth Token')
+            );
         }.bind(this))
 
         .then(function () {
@@ -107,7 +159,6 @@ Generator.prototype.writeDotTravisFile = function () {
                 return q.reject(err);
             }
         }.bind(this))
-        .then(this.celebrate('Write Travis Configuration File'), this.mourn('Write Travis Configuration File'))
 
         .then(function () {
             done();
